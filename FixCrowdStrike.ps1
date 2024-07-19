4a012d1090e5e5fa30453475fef9a8f14b3bb286
lucaskowe <lucas.kowe@gmail.com>
2024-07-19 21:37:51 +1000

    Update FixCrowdStrike.ps1

--- FixCrowdStrike.ps1
+++ FixCrowdStrike.ps1
@@ -1,10 +1,14 @@
 #Command/Script to fix the crowdstrike update that effected the world on 19/7/24 run in SAFEMODE as ADMIN
 #Author: Lucas Kowe
 #Remove problematic driver file (update(?))) thoroughly
-Remove-Item -Path "C:\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" -Force -Recurse -ErrorAction SilentlyContinue -Confirm:$false
-#Verify file no longer exists and is removed
-if (-not (Test-Path "C:\Windows\System32\drivers\CrowdStrike\C-00000291*.sys")) {
-    Write-Host "Crowdstrike Fix Applied Successfully Good Luck!"
-} else {
-    Write-Host "Crowdstrike Fix Failed, Something Went Wrong.."
+try {
+    Remove-Item -Path "C:\Windows\System32\drivers\CrowdStrike\C-00000291*.sys" -Force -Recurse -ErrorAction Stop -Confirm:$false
+    # Verify file no longer exists and is removed
+    if (-not (Test-Path "C:\Windows\System32\drivers\CrowdStrike\C-00000291*.sys")) {
+        Write-Host "Crowdstrike Fix Applied Successfully. Good Luck!"
+    } else {
+        Write-Host "Crowdstrike Fix Failed. Something Went Wrong.."
+    }
+} catch {
+    Write-Host "An error occurred while removing the file: $_"
 }
